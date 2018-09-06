e7780e46f7e8932fbc6724472277759a56d66aa8
Ian Davidson <bontaq@gmail.com>
2018-09-06 01:02:08 -0400

    Baby steps.

--- toogle/app/Main.hs
+++ toogle/app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
+import System.Process
+
 import Lib
 
+typeScriptLocation =
+  "/home/ian/code/jupiter/node_modules/typescript/bin/tsserver"
+
 main :: IO ()
-main = someFunc
+main = do
+  (a, hout, b, c) <-
+    createProcess (proc "ls" []){ std_out = CreatePipe }
+
+  print $ hout
+  print $ a
+  print $ b
+  -- print $ c
+
+  pure ()
